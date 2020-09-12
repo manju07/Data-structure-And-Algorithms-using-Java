cd024a782c3f4d7f4f19e07f7cadf7f55b543175
masundi <masundi@nouveau-labs.com>
2020-09-12 12:22:09 +0530

    count substring having equal number of 0's, 1's and 2's

---
+++ datastructure/hashmap/hard/CountSubStringsWithEqual0s1s2s.java
@@ -0,0 +1,41 @@
+package datastructure.hashmap.hard;
+
+import java.util.HashMap;
+import java.util.Map;
+import javafx.util.Pair;
+
+/**
+ * @author Manjunath Asundi
+ * Count substrings having equal number of o's, 1's and 2's
+ */
+public class CountSubStringsWithEqual0s1s2s {
+
+    public static int countSubStringsWithEqual0s1s2s(String str) {
+        int zc = 0, oc = 0, tc = 0, substrCount = 0;
+        Map<Pair<Integer, Integer>, Integer> map = new HashMap<Pair<Integer, Integer>, Integer>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '0')
+                zc++;
+            else if (str.charAt(i) == '1')
+                oc++;
+            else
+                tc++;
+            Pair<Integer, Integer> pair = new Pair<Integer, Integer>(zc - oc, zc - tc);
+            if (!map.containsKey(pair))
+                map.put(pair, 1);
+            else {
+                int count = map.get(pair);
+                substrCount += count;
+                map.put(pair, count + 1);
+            }
+        }
+        return substrCount;
+    }
+
+    public static void main(String[] args) {
+        String[] arrayOfStrings = { "010120102", "11112202101", "120121120" };
+        for (String string : arrayOfStrings)
+            System.out.println(string + " Count of substrings having equal number of 0's 1's and 2's = "
+                    + countSubStringsWithEqual0s1s2s(string));
+    }
+}
