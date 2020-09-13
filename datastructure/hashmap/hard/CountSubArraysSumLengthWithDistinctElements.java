85c93bdfa1da1bf7838a46ece097f32481ca3e4f
masundi <masundi@nouveau-labs.com>
2020-09-13 17:53:42 +0530

    Count all sub arrays sum length which subarrays have distinct elements

---
+++ datastructure/hashmap/hard/CountSubArraysSumLengthWithDistinctElements.java
@@ -0,0 +1,33 @@
+package datastructure.hashmap.hard;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Manjunath Asundi
+ * Count all sub arrays sum length which subarrays have distinct elements
+ */
+public class CountSubArraysSumLengthWithDistinctElements {
+
+    public static int countTotalSubArraysSumLengthWithDistinctElements(int arr[]) {
+        Set<Integer> set = new HashSet<>();
+        int countSubArraysLength = 0;
+        int j = 0, n = arr.length;
+        for (int i = 0; i < n; i++) {
+            while (j < n && !set.contains(arr[j]))
+                set.add(arr[j++]);
+            int count = ((j - i) * (j - i + 1)) / 2;
+            countSubArraysLength += count;
+            set.remove(arr[i]);
+        }
+        return countSubArraysLength;
+    }
+
+    public static void main(String[] args) {
+        int twoDArray[][] = { { 1, 2, 3 }, { 1, 2, 1 }, { 1, 2, 3, 4 } };
+        for (int[] arr : twoDArray) {
+            System.out.println("Count sum of sub arrays length with distinct elements = "
+                    + countTotalSubArraysSumLengthWithDistinctElements(arr));
+        }
+    }
+}
