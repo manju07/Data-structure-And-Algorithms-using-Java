912fe15d12321833f0d58da97d0ba25d13ea2965
masundi <masundi@nouveau-labs.com>
2020-10-01 08:59:14 +0530

    find nth fibonacchi number using dynamic programming

---
+++ algorithms/dynamicprogramming/Fibbonacci.java
@@ -0,0 +1,28 @@
+package algorithms.dynamicprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Manjunath Asundi
+ * Find nth Fibbonacci number by Dynamic programming 
+ */
+public class Fibbonacci {
+    public static int fibbonacciOfN(int n, Map<Integer, Integer> map) {
+        if (n <= 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+        if (map.containsKey(n))
+            return map.get(n);
+        int d = fibbonacciOfN(n - 1, map) + fibbonacciOfN(n - 2, map);
+        map.put(n, d);
+        return d;
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int n : new int[] { 10, 7, 5, 6 })
+            System.out.println(n + "th fibbonacci number is " + fibbonacciOfN(n, map));
+    }
+}
