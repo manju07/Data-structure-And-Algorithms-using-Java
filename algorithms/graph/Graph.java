2afff2d00e001f0a8a6591398ab72999b5363475
masundi <masundi@nouveau-labs.com>
2020-10-02 12:44:42 +0530

    Graph class updated including visited edges in the graph

--- algorithms/graph/Graph.java
+++ algorithms/graph/Graph.java
@@ -3,11 +3,12 @@ package algorithms.graph;
 import java.util.LinkedList;
 
 /**
- * @author Manjunath Asundi 
+ * @author Manjunath Asundi
  */
 public class Graph {
     private int v;
     private LinkedList<LinkedList<Integer>> adjList;
+    private Boolean visited[];
 
     public Graph() {
 
@@ -16,9 +17,11 @@ public class Graph {
     public Graph(int v) {
         this.v = v;
         adjList = new LinkedList<LinkedList<Integer>>();
-        for (int i = 0; i < v; i++) {
+        for (int i = 0; i < v; i++)
             adjList.add(new LinkedList<Integer>());
-        }
+        visited = new Boolean[this.v];
+        for (int i = 0; i < visited.length; i++)
+            visited[i] = false;
     }
 
     public void addEdge(int u, int v) {
@@ -31,5 +34,14 @@ public class Graph {
 
     public LinkedList<LinkedList<Integer>> getAdjList() {
         return this.adjList;
+    }
+
+    public Boolean[] getVisited() {
+        return visited;
+    }
+
+    public void resetVisited() {
+        for (int i = 0; i < visited.length; i++)
+            visited[i] = false;
     }
 }
