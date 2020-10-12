1250096cbbec542d8e9c4c9eece1bb7f1d08445a
masundi <masundi@nouveau-labs.com>
2020-10-12 10:55:40 +0530

    Edge class added, this has source, destination and weight fields

---
+++ algorithms/graph/Edge.java
@@ -0,0 +1,14 @@
+package algorithms.graph;
+
+/**
+ * Edge
+ */
+public class Edge {
+
+    int source, destination, weight;
+    public Edge(int source, int destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
