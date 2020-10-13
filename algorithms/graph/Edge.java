bde5072bd7eeb95248e6910d8c4ed6635dec5dc2
masundi <masundi@nouveau-labs.com>
2020-10-13 09:21:43 +0530

    setters and getters defined for edge class fields

--- algorithms/graph/Edge.java
+++ algorithms/graph/Edge.java
@@ -1,14 +1,39 @@
 package algorithms.graph;
 
 /**
- * Edge
+ * Edge class
+ * @author Manjunath Asundi
  */
 public class Edge {
 
-    int source, destination, weight;
+    private int source, destination, weight;
     public Edge(int source, int destination, int weight) {
         this.source = source;
         this.destination = destination;
+        this.weight = weight;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public void setSource(int source) {
+        this.source = source;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+
+    public void setDestination(int destination) {
+        this.destination = destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
         this.weight = weight;
     }
 }
