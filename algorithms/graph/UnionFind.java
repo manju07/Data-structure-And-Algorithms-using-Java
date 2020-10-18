38c91eff4b4d91c0b24e67893610b9b80beeef6e
masundi <masundi@nouveau-labs.com>
2020-10-18 09:34:12 +0530

    Union-Find algorithm to detect cycle in the graph

---
+++ algorithms/graph/UnionFind.java
@@ -0,0 +1,55 @@
+package algorithms.graph;
+
+/**
+ * @author Manjunath Asundi
+ * Union-Find algorithm to detect cycle in the graph
+ */
+public class UnionFind {
+    public static void union(int parent[], int u, int v) {
+        int u1 = find(parent, u);
+        int v1 = find(parent, v);
+        parent[u1] = v1;
+    }
+
+    public static int find(int parent[], int v) {
+        if (parent[v] == -1)
+            return v;
+        return find(parent, parent[v]);
+    }
+
+    public static Boolean isCycleDetected(int V, int E, Edge[] edges) {
+        int parent[] = new int[V];
+        for (int i = 0; i < parent.length; i++)
+            parent[i] = -1;
+
+        for (int i = 0; i < E; i++) {
+            int s = find(parent, edges[i].getSource());
+            int d = find(parent, edges[i].getDestination());
+            if (s == d)
+                return true;
+            union(parent, s, d);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int V = 4, E = 3;
+        Edge[] edges = new Edge[3];
+        for (int i = 0; i < edges.length; i++)
+            edges[i] = new Edge();
+
+        edges[0].setSource(0);
+        edges[0].setDestination(1);
+
+        edges[1].setSource(1);
+        edges[1].setDestination(2);
+
+        edges[2].setSource(2);
+        edges[2].setDestination(3);
+        if (isCycleDetected(V, E, edges))
+            System.out.println("Cycle Detected");
+        else
+            System.out.println("There is no cycle found");
+
+    }
+}
