6196779897448977b6b2cb0b020a465d167bb722
m0a07xh <manjunath.asundi@walmartlabs.com>
2021-03-21 10:20:58 +0530

    Deadlock situation

---
+++ multithreads/Deadlock.java
@@ -0,0 +1,70 @@
+package multithreads;
+
+/**
+ * SharedObject1
+ */
+class SharedObject1 {
+
+    public synchronized void method1(SharedObject1 so1, SharedObject2 so2) {
+        System.out.println("method1 executing, current thread:" + Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        so2.method2(so1, so2);
+    }
+}
+
+/**
+ * SharedObject2
+ */
+class SharedObject2 {
+
+    public synchronized void method2(SharedObject1 so1, SharedObject2 so2) {
+        System.out.println("method2 executing, current thread:" + Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        so1.method1(so1, so2);
+    }
+}
+
+/**
+ * Deadlock
+ * 
+ * @author Manjunath Asundi
+ */
+
+public class Deadlock {
+
+    public static void main(String[] args) {
+
+        SharedObject1 so1 = new SharedObject1();
+        SharedObject2 so2 = new SharedObject2();
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                so1.method1(so1, so2);
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                so2.method2(so1, so2);
+            }
+        });
+        t1.start();
+        try {
+            // t1.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        t2.start();
+    }
+}
