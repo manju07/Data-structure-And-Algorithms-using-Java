0a25cd023daf900d2782b7dbe989294b7e3d9b55
m0a07xh <manjunath.asundi@walmartlabs.com>
2021-03-21 11:35:50 +0530

    updated code for interthread communication

--- multithreads/InterThreadCommunication.java
+++ multithreads/InterThreadCommunication.java
@@ -5,31 +5,45 @@ package multithreads;
  */
 class Shared {
     private int amount = 10;
+    private int iteration = 10, iteration2 = 10;
 
     public synchronized void withDraw(int withDrawMoney) {
-        if (amount < withDrawMoney) {
-            System.out.println("Balance is less, wait for deposit");
+        while (iteration != 0) {
+            if (amount < withDrawMoney) {
+                System.out.println("Balance is less, wait for deposit");
+                try {
+                    wait();
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+            }
+            amount -= withDrawMoney;
+            System.out.println("Amount withdrawn " + withDrawMoney + " succeessfully, balance amount:" + amount);
+            notify();
+            iteration--;
+        }
+    }
+
+    public synchronized void deposit(int depositMoney) {
+        while (iteration2 != 0 && iteration!=0) {
+            amount += depositMoney;
+            System.out.println("Amount deposited " + depositMoney + " succeessfully, balance amount:" + amount);
+            notify();
             try {
                 wait();
             } catch (Exception e) {
                 // TODO: handle exception
-                e.printStackTrace();
             }
-        } 
-        amount -= withDrawMoney;
-        System.out.println("Amount withdrawn succeessfully, balance amount:" + amount);
-    }
-
-    public synchronized void deposit(int depositMoney) {
-        amount += depositMoney;
-        System.out.println("Amount deposited succeessfully, balance amount:" + amount);
-        notify();
+            iteration2--;
+        }
     }
 
 }
 
 /**
  * Interthread communication
+ * 
  * @author Manjunath Asundi
  */
 public class InterThreadCommunication {
@@ -46,7 +60,7 @@ public class InterThreadCommunication {
 
             @Override
             public void run() {
-                s1.deposit(200);
+                s1.deposit(120);
             }
         });
         t1.start();
