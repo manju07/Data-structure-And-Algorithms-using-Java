697263a6f133703f06d14281d67107d8f1a5a523
m0a07xh <manjunath.asundi@walmartlabs.com>
2021-05-08 13:36:44 +0530

    ParsingProgram modified

--- files/ParsingProgram.java
+++ files/ParsingProgram.java
@@ -3,6 +3,8 @@ package files;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
 
 /**
@@ -11,52 +13,79 @@ import java.util.stream.Collectors;
  */
 public class ParsingProgram {
 
-    static int blankCount = 0, commentsCount = 0, codeCount = 0, totalLinesCount = 0, importCount = 0, variableDeclarationCount = 0;
+    /**
+     * ParseData
+     */
+    static class ParseData implements Runnable {
 
-    public static void readFile(String fileContent) {
-        String lines[] = fileContent.split("\n");
-        totalLinesCount = totalLinesCount + lines.length;
-        for (int i = 0; i < lines.length; i++) {
-            String line = lines[i].trim();
-            if (line.isEmpty())
-                blankCount++;
-            else if (line.startsWith("//") || line.startsWith("/*")) {
-                if (line.startsWith("/*")) {
-                    while (i < lines.length) {
-                        if (lines[i].endsWith("*/"))
-                            break;
-                        i++;
-                        commentsCount++;
-                    }
-                }
-                commentsCount++;
-            } else {
-                if(line.startsWith("import")) {
-                    importCount++;
-                } else if(line.startsWith("int") || line.startsWith("float") || line.startsWith("char") || line.startsWith("String"))
-                    variableDeclarationCount++;
-                codeCount++;
+        static int blankCount = 0, commentsCount = 0, codeCount = 0, totalLinesCount = 0, importCount = 0, variableDeclarationCount = 0;
+        static String fileContent = null;
+        public ParseData(String data) {
+            ParseData.fileContent = data;
+        }
+
+        @Override
+        public void run() {
+            try {
+                readFile();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+        }
+
+        public static void readFile() {
+            String lines[] = fileContent.split("\n");
+            totalLinesCount = totalLinesCount + lines.length;
+            for (int i = 0; i < lines.length; i++) {
+                String line = lines[i].trim();
+                if (line.isEmpty())
+                    blankCount++;
+                else if (line.startsWith("//") || line.startsWith("/*")) {
+                    if (line.startsWith("/*")) {
+                        while (i < lines.length) {
+                            if (lines[i].endsWith("*/"))
+                                break;
+                            i++;
+                            commentsCount++;
+                        }
+                    }
+                    commentsCount++;
+                } else {
+                    if(line.startsWith("import")) {
+                        importCount++;
+                    } else if(line.startsWith("int") || line.startsWith("float") || line.startsWith("char") || line.startsWith("String"))
+                        variableDeclarationCount++;
+                    codeCount++;
+                }
+            }
+        }
+
+        public ParseData() {
         }
     }
 
+
     public static void main(String[] args) {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(20);
         try {
-            File file = new File("/Users/m0a07xh/Projects/Data-structure-And-Algorithms-using-Java/");
+            File file = new File("/Users/m0a07xh/Projects/Data-structure-And-Algorithms-using-Java/files");
             String[] filesList = file.list();
             for (int i = 0; i < filesList.length; i++) {
                 if (filesList[i].endsWith(".java")) {
                     System.out.println(filesList[i]);
                     String data = Files
                             .lines(Paths.get(
-                                    "/Users/m0a07xh/Projects/Data-structure-And-Algorithms-using-Java/" + filesList[i]))
+                                    "/Users/m0a07xh/Projects/Data-structure-And-Algorithms-using-Java/files/" + filesList[i]))
                             .collect(Collectors.joining(System.lineSeparator()));
                     // System.out.println(data);
-                    readFile(data);
+                    executor.execute(new ParseData(data));
                 }
             }
-            System.out.println("blankCount:" + blankCount + " commentsCount:" + commentsCount + " codeCount:"
-                    + codeCount + " totalLinesCount:" + totalLinesCount);
+            executor.shutdown();
+            while (!executor.isTerminated()) {
+            }
+            System.out.println("blankCount:" + ParseData.blankCount + " commentsCount:" + ParseData.commentsCount + " codeCount:"
+                    + ParseData.codeCount + " totalLinesCount:" + ParseData.totalLinesCount);
         } catch (Exception e) {
             e.printStackTrace();
         }
