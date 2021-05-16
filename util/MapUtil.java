7a9ddac3d4e3f3ed70f898b7103f463a5d820e26
m0a07xh <manjunath.asundi@walmartlabs.com>
2021-05-16 12:50:47 +0530

    Map related util methods are written here

--- util/MapUtil.java
+++ util/MapUtil.java
@@ -4,14 +4,13 @@ import java.util.Map;
 
 /**
  * Map related util methods are written here
+ * 
  * @author Manjunath Asundi
  */
 public class MapUtil {
 
-    private static void printMap(Map<Object, Object> map) {
-        for (Map.Entry<Object, Object> data : map.entrySet()) {
-            System.out.println(data.getKey() + " " + data.getValue());
-        }
+    public static void printMap(Map map) {
+        map.forEach((k, v) -> System.out.println(k + " " + v));
         System.out.println();
     }
 }
