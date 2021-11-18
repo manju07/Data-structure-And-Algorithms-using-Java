3f09dd0dae3071cc3eb30ca7c7a18d57c100a4ba
manjunatha <manjunatha@zeta.tech>
2021-11-18 11:29:24 +0530

    Find Maximum sum of subarray having unique elements

---
+++ datastructure/array/MaximumSumOfUniqueElementsSubarray.java
@@ -0,0 +1,38 @@
+package datastructure.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Find Maximum sum of subarray having unique elements
+ * 
+ * @author Manjunath Asundi
+ */
+public class MaximumSumOfUniqueElementsSubarray {
+
+    public static int findMaxSumSubarray(int arr[]) {
+        int i = 0, j = 1;
+        int maxSum = Integer.MIN_VALUE, sum = arr[i];
+        Set<Integer> set = new HashSet<>();
+        set.add(arr[i]);
+        for (; i < arr.length && j < arr.length;) {
+            if (set.contains(arr[j])) {
+                sum = sum - arr[i];
+                set.remove(arr[i]);
+                i++;
+            } else {
+                set.add(arr[j]);
+                sum = sum + arr[j];
+                maxSum = Math.max(sum, maxSum);
+                j++;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        for (int arr[] : new int[][] { { 1, 2, 3, 3, 1, 5, 6 }, { 4, 2, 3, 1, 5, 6, 7, 2 },
+                { 5, 6, 3, 4, 1, 2, 3, 8, 7 } })
+            System.out.println("Max Sum = " + findMaxSumSubarray(arr));
+    }
+}
