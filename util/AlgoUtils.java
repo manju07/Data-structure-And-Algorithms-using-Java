de3bb19396c7df52b5bb055449b717a40575b5ed
m0a07xh <manjunath.asundi@walmart.com>
2022-12-25 10:49:53 +0530

    Util files updated.

--- util/AlgoUtils.java
+++ util/AlgoUtils.java
@@ -1,10 +1,21 @@
 package util;
 
+import java.util.Random;
+
 public class AlgoUtils {
 
     public static void swapByIndex(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    /**
+     * @apiNote Return random value between o to bound range
+     * @param bound
+     * @return random value between 0 to bound range
+     */
+    public static int random(int bound) {
+        return new Random().nextInt(bound);
     }
 }
--- util/ArrayUtil.java
+++ util/ArrayUtil.java
@@ -1,6 +1,8 @@
 package util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Array related Util Methods
@@ -25,5 +27,37 @@ public class ArrayUtil<T extends Number> {
         for (Object object : list)
             System.out.print(object + " ");
         System.out.println();
+    }
+
+    /**
+     * Generate random unique elements integer array of size between 5 and 10
+     * and value will be in range of 0 to 15
+     * 
+     * @return int array
+     */
+    public static int[] constructRandomUniqueElementsArray() {
+        int low = 5;
+        int length = AlgoUtils.random(5) + low;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < length; i++) {
+            set.add(AlgoUtils.random(15));
+        }
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    /**
+     * Generate random integer array of size between 5 and 10
+     * and value will be in range of 0 to 15
+     * 
+     * @return int array
+     */
+    public static int[] constructRandomArray() {
+        int low = 5;
+        int length = AlgoUtils.random(5) + low;
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = AlgoUtils.random(15);
+        }
+        return arr;
     }
 }
