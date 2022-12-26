340c67385be1ff026e834dff939a2ae06e4b2f76
m0a07xh <manjunath.asundi@walmart.com>
2022-12-26 13:24:03 +0530

    Solution-2 for Find all triangles count that a + b > c, a + c > b, b + c > a

--- datastructure/array/Traingles.java
+++ datastructure/array/Traingles.java
@@ -32,6 +32,35 @@ public class Traingles {
         return count;
     }
 
+    /**
+     * @apiNote Find all traingles
+     *          Time complexity is O(n^2) and space complexity is O(1)
+     * @param arr Integer Array
+     * @return count of traingles
+     */
+    private static int findAllTrainglesSolution2(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return 0;
+
+        int count = 0;
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        for (int i = n - 1; i > 1; i--) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (arr[l] + arr[r] > arr[i]) {
+                    count += (r - l);
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         int[][] twoDArr = {
                 ArrayUtil.constructRandomUniqueElementsArray(),
@@ -43,9 +72,29 @@ public class Traingles {
                 { 4, 6, 3, 7 },
                 { 10, 21, 22, 100, 101, 200, 300 }
         };
+
+        System.out.println("\n\nSolution - 1\n");
         for (int[] arr : twoDArr) {
             ArrayUtil.printArray(arr);
             System.out.println("Number of traingles = " + findAllTraingles(arr));
+            System.out.println();
+        }
+
+        twoDArr = new int[][] {
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructArrayWithinRange(10),
+                { 8, 4, 6, 3, 7, 9 },
+                { 4, 6, 3, 7 },
+                { 10, 21, 22, 100, 101, 200, 300 }
+        };
+
+        System.out.println("\n\nSolution - 2\n");
+        for (int[] arr : twoDArr) {
+            ArrayUtil.printArray(arr);
+            System.out.println("Number of traingles = " + findAllTrainglesSolution2(arr));
             System.out.println();
         }
     }
