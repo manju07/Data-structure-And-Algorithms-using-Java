be83f38da8f63d9d97a1c7f9f2f218e13c153229
m0a07xh <manjunath.asundi@walmart.com>
2022-12-26 13:06:15 +0530

    Find all triangles count that a + b > c, a + c > b, b + c > a

---
+++ datastructure/array/Traingles.java
@@ -0,0 +1,52 @@
+package datastructure.array;
+
+import java.util.Arrays;
+
+import util.ArrayUtil;
+
+/**
+ * Find all triangles count that a + b > c, a + c > b, b + c > a
+ * 
+ * @author Manjunath Asundi
+ */
+public class Traingles {
+
+    private static int findAllTraingles(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return 0;
+
+        int count = 0;
+        int n = arr.length;
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 2; i++) {
+            int k = i + 2;
+            for (int j = i + 1; j < n - 1; j++) {
+                while ((k < n) && (arr[i] + arr[j] > arr[k])) {
+                    k++;
+                }
+                if (k > j) {
+                    count += (k - j - 1);
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[][] twoDArr = {
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructRandomUniqueElementsArray(),
+                ArrayUtil.constructArrayWithinRange(10),
+                { 8, 4, 6, 3, 7, 9 },
+                { 4, 6, 3, 7 },
+                { 10, 21, 22, 100, 101, 200, 300 }
+        };
+        for (int[] arr : twoDArr) {
+            ArrayUtil.printArray(arr);
+            System.out.println("Number of traingles = " + findAllTraingles(arr));
+            System.out.println();
+        }
+    }
+}
